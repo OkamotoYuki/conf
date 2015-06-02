f264c1c6c64a55c051f6a7e562f7ed594d61491e
OkamotoYuki <okamoto.yuki.0130@gmail.com>
2015-06-02 09:11:51 +0900

    add copy-from.zsh

---
+++ copy-from.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# zsh
+cp .zshrc ~
+
+# vim
+cp .vimrc ~
+cp .vimrc.local ~
+cp .vimrc.local.bundles ~
+
+# git
+cp .gitconfig ~
+
+# tmux
+cp .tmux.conf ~
+
+# vimperator
+cp .vimperatorrc ~
